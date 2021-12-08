f17f0958a453efc54f17f20c9563f7e5a7e7e7f9
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-12-07 21:21:07 -0300

    commit

--- core/Tocador.js
+++ core/Tocador.js
@@ -1,5 +1,19 @@
 let musica = document.querySelector('audio')
-document.getElementById('.bnt-play').addEventListener('click')
+let musicas = []
+
+
+let tocando = false
 function tocarMusica(){
     musica.play()
+    tocando=true
+    document.getElementById('btn-play').style.display = 'none'
+    document.querySelector('.btn-pause').style.display = 'block'
+    document.getElementById('btn-pause').style.display = 'block'
+}
+function pausar(){
+    musica.pause()
+    tocando = false
+    document.getElementById('btn-play').style.display = 'block'
+    document.querySelector('.btn-pause').style.display = 'none'
+    document.getElementById('btn-pause').style.display = 'none'
 }
